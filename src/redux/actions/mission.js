93ab4d9eef1db8d37cea7c9a4509797b51533298
Masoudi Parrot <masoudi@localhost.localdomain>
2022-05-11 00:44:10 -0400

    Switch badges for mission

--- src/redux/actions/mission.js
+++ src/redux/actions/mission.js
@@ -1,4 +1,7 @@
+import local from "../../components/localStore";
+
 const FETCH_MISSION_SUCCEED = 'src/redux/mission/FETCH_MISSION_SUCCEED';
+const JOIN_MISSION = 'src/redux/mission/JOIN_MISSION';
 const link = 'https://api.spacexdata.com/v3/missions';
 const initialState = {
   Missions: [],
@@ -9,12 +12,35 @@ export const fetchMissionSuceed = (missions) => ({
   missions,
 });
 
+export const joinMission = (id) => ({
+  type: JOIN_MISSION,
+  id,
+});
+
 const missionReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_MISSION_SUCCEED:
       return {
         ...state,
         Missions: action.missions,
+      }
+
+    case JOIN_MISSION:
+      const newtable = state.Missions.map((mission) => {
+        if( action.id === mission.mission_id) {
+          return {
+            mission_id: mission.mission_id,
+            mission_name: mission.mission_name,
+            description: mission.description,
+            status: !mission.status,
+          };
+        } else {
+          return mission;
+        }
+      })
+      return {
+        ...state,
+        Missions:[...newtable],
       }
 
     default:
@@ -24,6 +50,7 @@ const missionReducer = (state = initialState, action) => {
 
 export const fetchMission = () => (
   function (dispatch) {
+    const Lstore = local('missions')
     fetch(link)
       .then((response) => response.json())
       .then((data) => {
@@ -32,7 +59,7 @@ export const fetchMission = () => (
           mission_id,
           mission_name,
           description,
-          status: false,
+          status: (Lstore.includes(mission_id)) ? true : false,
         }));
         dispatch(fetchMissionSuceed(formaData))});
   }
